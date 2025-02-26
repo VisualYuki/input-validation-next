a2f06321b193f0efacd0ff527eef84532e8b6757
denis moshkin <denis.moshkin.007@yandex.ru>
2025-02-26 12:16:27 +0300

    fix ci cd

--- src/locale/messages_zh.ts
+++ src/locale/messages_zh.ts
@@ -2,11 +2,10 @@ import type {Messages} from "@/common";
 
 export const messages_zh: Messages = {
 	required: "这是必填字段",
-	maxlength: "最多可以输入 {0} 个字符",
-	minlength: "最少要输入 {0} 个字符",
+	maxLength: "最多可以输入 {0} 个字符",
+	minLength: "最少要输入 {0} 个字符",
 	email: "请输入有效的电子邮件地址",
 	url: "请输入有效的网址",
-	date: "请输入有效的日期",
 	number: "请输入有效的数字",
 	digits: "只能输入数字",
 	equalTo: "你的输入不相同",
